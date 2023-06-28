bdfc75e5e14aa6cb76e279aed41503a128d1ec56
Reshma-Ghosh <reshmaghosh114@gmail.com>
2023-06-28 14:35:24 +0200

    Express-Routes

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,10 +2,24 @@
 
 import { Request, Response } from "express";
 
+const userList = [
+  { id: 1, name: "Reshma", age: 1 },
+  { id: 2, name: "Sourina", age: 2 },
+  { id: 3, name: "Abhi", age: 3 },
+  { id: 4, name: "Gayathri", age: 4 },
+];
+
 export const createUsers = async (request: Request, response: Response) => {
   response.json({ message: "create users" });
 };
 
 export const deleteUser = async (request: Request, response: Response) => {
   response.json({ message: "delete user" });
+};
+
+export const createUsersById = async (request: Request, response: Response) => {
+  const userId = request.params;
+  console.log(userId, "user id");
+  const result = userList.filter((user) => user.id === Number(userId.id));
+  response.json(result);
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,14 @@
 // product routes here
 
-import { Router, Request, Response } from "express";
-import { createUsers, deleteUser } from "../controllers/users";
+import { Router } from "express";
+import { createUsers, createUsersById, deleteUser } from "../controllers/users";
 
 const router = Router();
 
 router.post("/", createUsers);
 
 router.delete("/", deleteUser);
+
+router.post("/:id", createUsersById);
 
 export default router;
